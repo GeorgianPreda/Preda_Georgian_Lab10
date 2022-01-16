e8b4d42723513b26014f22dd4f4fdbdf3643b328
Georgian Preda <predageorgian8@gmail.com>
2022-01-16 23:51:37 +0200

    Add project files.

---
+++ Preda_Georgian_Lab10.Android/Models/ShopList.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SQLite;
+
+namespace Preda_Georgian_Lab10.Droid.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
